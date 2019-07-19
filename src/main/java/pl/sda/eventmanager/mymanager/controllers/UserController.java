e18c47259f6e04f80475ef6cc29ba84521a0f812
krzysztof sabatowski <krzysztof.sabatowski@gmail.com>
2019-07-19 23:40:02 +0200

    fixing network of login and user pages, delete CSV file, comment some dependecies in pom.xml

--- src/main/java/pl/sda/eventmanager/mymanager/controllers/UserController.java
+++ src/main/java/pl/sda/eventmanager/mymanager/controllers/UserController.java
@@ -3,6 +3,7 @@ package pl.sda.eventmanager.mymanager.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -11,6 +12,8 @@ import org.springframework.web.servlet.ModelAndView;
 import pl.sda.eventmanager.mymanager.dto.LoginForm;
 import pl.sda.eventmanager.mymanager.repository.UserRepository;
 import pl.sda.eventmanager.mymanager.service.UserService;
+
+import java.util.Arrays;
 
 @Controller
 @ComponentScan("pl.sda.eventmanager.mymanager")
@@ -36,15 +39,14 @@ public class UserController {
     }
 
     @GetMapping("youAreLoggedIn")
-    public ModelAndView logForm(Authentication authentication){
+    public ModelAndView logForm(/*Authentication authentication*/){
 
-        System.out.println("getAuthorities: " + authentication.getAuthorities());
-        System.out.println("getPrincipal: " + authentication.getPrincipal());
-        System.out.println("getDetails: " + authentication.getDetails());
-        System.out.println("isAuthenticated: " + authentication.isAuthenticated());
-
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         ModelAndView modelAndView = new ModelAndView("youAreLoggedIn"/*, "loginForm", new LoginForm()*/);
-//        modelAndView.addObject("nick", Arrays.asList(authentication.getPrincipal()));
+        modelAndView.addObject(authentication.getPrincipal());
+        // ta linijka nic nie robi oprocz wyswietlania informacji o widoku "login"
+        System.out.println(loginInForm().addObject(modelAndView));
+
         return modelAndView;
     }
 
